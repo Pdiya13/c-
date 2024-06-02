aac294573a31e5bc927f42d2c2353f003478f117
Pdiya13 <pdiya2945@gmail.com>
2024-06-03 04:26:53 +0530

    null

---
+++ ch18_2D_Arrays/staircase_sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m,n;
+    cout<<"enter m & n:"; 
+    cin>>m>>n;
+    int arr[m][n];
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    int key;
+    cout<<"enter number you want to search:"<<endl;
+    cin>>key;
+
+    int r=0,c=n-1;
+    while(r<m&&c>=0)
+    {
+        if(arr[r][c]==key)
+        {
+            cout<<"present"<<r<<" "<<c<<endl;
+            break;
+        }
+        else if(arr[r][c]>key){
+            c--;
+        }
+        else if(arr[r][c]<key)
+        {
+            r++;
+        }
+    }
+    
+    return 0;
+}
